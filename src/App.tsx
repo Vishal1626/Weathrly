e70e746e9df546c6ef93d75a44d956edfb18d24f
Jadhav <vishal.jadhav@ingrammicro.com>
2025-02-19 12:55:24 +0530

    Add lazy loading to fix vecel deployment issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import "./App.css";
 import Layout from "./components/layout";
 import { ThemeProvider } from "./context/theme-provide";
-import WeatherPage from "./pages/weather-page";
-import CityPage from "./pages/city-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import LoadingSkeleton from "./components/LoadingSkeleton";
+
+// Lazy load pages
+const WeatherPage = lazy(() => import("./pages/weather-page"));
+const CityPage = lazy(() => import("./pages/city-page"));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 5 * 60 * 1000,
-        gcTime: 10 * 60 * 1000,
-        retry: false,
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <ThemeProvider defaultTheme="dark">
           <Layout>
-            <Routes>
-              <Route path="/" element={<WeatherPage />} />
-              <Route path="/city/:cityName" element={<CityPage />} />
-            </Routes>
+            {/* Suspense fallback UI while components are loading */}
+            <Suspense fallback={<LoadingSkeleton />}>
+              <Routes>
+                <Route path="/" element={<WeatherPage />} />
+                <Route path="/city/:cityName" element={<CityPage />} />
+              </Routes>
+            </Suspense>
           </Layout>
         </ThemeProvider>
       </BrowserRouter>
